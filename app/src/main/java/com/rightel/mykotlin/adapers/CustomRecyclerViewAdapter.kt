81634bafd71a08b1ebb518c4e8a5f9c7b5d1790d
Mohammad Mirzakhani <m.mirzakhani@rightel.ir>
2018-10-27 16:26:24 +0330

    add RecyclerView custom adapter and related view row and Data model

---
+++ app/src/main/java/com/rightel/mykotlin/adapers/CustomRecyclerViewAdapter.kt
@@ -0,0 +1,33 @@
+package com.rightel.mykotlin.adapers
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rightel.mykotlin.R
+import com.rightel.mykotlin.dataModel.DataModelRecycler
+import kotlinx.android.synthetic.main.row_recycler.view.*
+
+class CustomRecyclerViewAdapter(var listModel : ArrayList<DataModelRecycler>) : RecyclerView.Adapter<CustomRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val view = LayoutInflater.from(p0.context).inflate(R.layout.row_recycler, p0,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listModel.size
+    }
+
+    override fun onBindViewHolder(vh: ViewHolder, index: Int) {
+       vh.txtTitle.text = listModel[index].name
+       vh.txtDate.text = listModel[index].date
+    }
+
+
+    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
+        val txtTitle = itemView.row_recycler_txt_title!!
+        val txtDate = itemView.row_recycler_txt_date!!
+    }
+
+}
---
+++ app/src/main/java/com/rightel/mykotlin/dataModel/DataModelRecycler.kt
@@ -0,0 +1,3 @@
+package com.rightel.mykotlin.dataModel
+
+data class DataModelRecycler (var name : String, var date : String)
