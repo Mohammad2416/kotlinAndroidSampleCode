eba2918b5f5eb9934a2a9f70c58dc777ee95f0a5
Mohammad Mirzakhani <m.mirzakhani@rightel.ir>
2018-10-29 16:47:14 +0330

    Add RxJava 2 and Retrofit 2 to Gradle

---
+++ app/src/main/java/com/rightel/mykotlin/RestApiActivity.kt
@@ -0,0 +1,12 @@
+package com.rightel.mykotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RestApiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rest_api)
+    }
+}
