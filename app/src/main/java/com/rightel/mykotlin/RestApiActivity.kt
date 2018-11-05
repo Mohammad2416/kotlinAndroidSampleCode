79991efe9b8a39ae013ff4ced2ed4f9c0852c688
Mohammad Mirzakhani <m.mirzakhani@rightel.ir>
2018-11-05 12:22:22 +0330

    fetch Data from internet with RxJava and Retrofit

--- app/src/main/java/com/rightel/mykotlin/RestApiActivity.kt
+++ app/src/main/java/com/rightel/mykotlin/RestApiActivity.kt
@@ -7,30 +7,18 @@ package com.rightel.mykotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.widget.LinearLayout
-import com.rightel.mykotlin.adapers.CustomRecyclerViewAdapter
 import com.rightel.mykotlin.adapers.PostApiRecyclerViewAdapter
 import com.rightel.mykotlin.dataModel.DataModelPost
-import com.rightel.mykotlin.retrofit.RestApis
 import com.rightel.mykotlin.retrofit.RetrofitClient
 import kotlinx.android.synthetic.main.activity_rest_api.*
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
 class RestApiActivity : AppCompatActivity() {
 
-    private lateinit var restApi : RestApis
-    private var retrofit: Retrofit? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rest_api)
-
-        restApi = getRetrofitInstance().create(RestApis::class.java)
 
         activity_restapi_rv.setHasFixedSize(true)
         activity_restapi_rv.layoutManager = LinearLayoutManager(this)
@@ -39,22 +27,10 @@ class RestApiActivity : AppCompatActivity() {
     }
 
     private fun fetchData() {
-        restApi.posts.subscribeOn(Schedulers.io())
+        RetrofitClient.getService().posts
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe{posts -> displayData(posts)}
-
-    }
-
-
-    private fun  getRetrofitInstance(): Retrofit {
-        if (retrofit == null) {
-            retrofit = retrofit2.Retrofit.Builder()
-                    .baseUrl("https://jsonplaceholder.typicode.com/")
-                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-        }
-        return retrofit!!
     }
 
     private fun displayData(posts: List<DataModelPost>?) {
--- app/src/main/java/com/rightel/mykotlin/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/rightel/mykotlin/retrofit/RetrofitClient.kt
@@ -4,19 +4,17 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitClient {
+class RetrofitClient {
 
-    private var retrofit: Retrofit? = null
-    fun  getRetrofitInstance(): Retrofit {
-        if (retrofit == null) {
-            retrofit = retrofit2.Retrofit.Builder()
+    companion object {
+        fun getService(): RestApis {
+            val retrofit = Retrofit.Builder()
                     .baseUrl("https://jsonplaceholder.typicode.com/")
                     .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
+            return retrofit.create(RestApis::class.java)
         }
-        return retrofit!!
+
     }
-
-
 }
